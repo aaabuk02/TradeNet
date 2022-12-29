cbf3673874d87a12325b75990be88202ef3f58b1
Abu <abubakarabukar@gmail.com>
2022-12-29 01:54:32 -0500

    Move buildGraph into useEffect

--- src/Tabs/Visualize/Visualize.js
+++ src/Tabs/Visualize/Visualize.js
@@ -1,52 +1,57 @@
 import React, { useState, useEffect, useContext } from "react";
 import CytoscapeComponent from "react-cytoscapejs";
 import { DataContext } from "../../App";
+
 const Visualize = (props) => {
   const { primaryChoice, secondaryChoice, edgesData, nodesData } =
     useContext(DataContext);
   const [reset, setReset] = useState(true);
-
-  useEffect(() => {
-    buildGraph(primaryChoice.value, secondaryChoice.value);
-  }, []);
   const [graphData, setGraphData] = useState({
     nodes: [],
     edges: [],
   });
 
-  function buildGraph(root, neighbors) {
-    // var queue = [];
-    // const visited = new Set();
-    var graph = {
-      nodes: [{ data: { id: root.Name, label: root.Name } }],
-      edges: [],
-    };
-    if (root.Name === "Default") {
-      return graph;
-    }
-    if (neighbors.Name === "Anybody") {
-      console.log(root);
-      var neighbor;
-      for (var i = 0; i < root.Edges.length; i++) {
-        console.log(edgesData[root.Edges[i]]);
-        var index = root.Edges[i];
-        if (edgesData[index].From === root.Name) {
-          neighbor = edgesData[index].To;
-        } else {
-          neighbor = edgesData[index].From;
-        }
-        graph.nodes.push({ data: { id: neighbor, label: neighbor } });
-        graph.edges.push({ data: { source: root.Name, target: neighbor } });
+  useEffect(() => {
+    function buildGraph(root, neighbors) {
+      // var queue = [];
+      // const visited = new Set();
+      var graph = {
+        nodes: [{ data: { id: root.Name, label: root.Name } }],
+        edges: [],
+      };
+      if (root.Name === "Default") {
+        return graph;
       }
-    } else {
-      console.log(nodesData);
-      graph.nodes.push({ data: { id: neighbors.Name, label: neighbors.Name } });
-      graph.edges.push({ data: { source: root.Name, target: neighbors.Name } });
+      if (neighbors.Name === "Anybody") {
+        console.log(root);
+        var neighbor;
+        for (var i = 0; i < root.Edges.length; i++) {
+          console.log(edgesData[root.Edges[i]]);
+          var index = root.Edges[i];
+          if (edgesData[index].From === root.Name) {
+            neighbor = edgesData[index].To;
+          } else {
+            neighbor = edgesData[index].From;
+          }
+          graph.nodes.push({ data: { id: neighbor, label: neighbor } });
+          graph.edges.push({ data: { source: root.Name, target: neighbor } });
+        }
+      } else {
+        console.log(nodesData);
+        graph.nodes.push({
+          data: { id: neighbors.Name, label: neighbors.Name },
+        });
+        graph.edges.push({
+          data: { source: root.Name, target: neighbors.Name },
+        });
+      }
+      setGraphData(graph);
+      reset ? setReset(false) : setReset(true);
+      // return graph;
     }
-    setGraphData(graph);
-    reset ? setReset(false) : setReset(true);
-    // return graph;
-  }
+
+    buildGraph(primaryChoice.value, secondaryChoice.value);
+  }, []);
 
   const layout = {
     name: "cose",
