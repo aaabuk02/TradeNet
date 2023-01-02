4b6fba80aa652594919d83a7497e797dc8a7e2e5
Abu <abubakarabukar@gmail.com>
2023-01-01 21:44:31 -0500

    Update about section

--- src/Tabs/About/About.js
+++ src/Tabs/About/About.js
@@ -1,11 +1,57 @@
-import { Text } from "@chakra-ui/react";
+import { Heading, Text, Grid, GridItem, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon, EmailIcon } from "@chakra-ui/icons";
 
 const About = () => {
   return (
     <div>
-      <Text fontSize="6xl" as="b">
-        Welcome To TradeNet 🏀
-      </Text>
+      <Grid templateColumns="1fr" gap={10} maxWidth="55rem">
+        <GridItem>
+          <Heading fontSize="5xl" as="b">
+            Welcome To TradeNet 🏀
+          </Heading>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="xl">
+            Trades are one of the most interesting parts about the NBA and can
+            have a domino effect for future transactions to come. This
+            application allows users to explore the history of NBA player
+            transactions by visualizing them as a graph. To build the graph,
+            simply select a player and (optionally) another player who has been
+            involved in a trade with them. You can also specify the number of
+            additional exchanges to include in the graph, allowing you to
+            control its size and complexity
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="xl">
+            Data source is from the amazing{" "}
+            <Link href="https://prosportstransactions.com/" isExternal>
+              prosportstransactions.com <ExternalLinkIcon mx="2px" />
+            </Link>
+            {" "}and is currently using data from 2021
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="xl">
+            Inspired by this {" "}
+            <Link
+              href="https://svitkin.shinyapps.io/bball-trade-network/"
+              isExternal
+            >
+              project{" "}
+              <ExternalLinkIcon mx="2px" />
+            </Link>
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Link href="https://github.com/aaabuk02/trade-net" isExternal>
+            Source Code <ExternalLinkIcon mx="2px" />
+          </Link><br/>
+          <Link href="mailto:aaabuk02@gmail.com" isExternal>
+            Contact Me <EmailIcon mx="2px" />
+          </Link>
+        </GridItem>
+      </Grid>
     </div>
   );
 };
