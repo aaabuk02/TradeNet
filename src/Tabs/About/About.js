a02824097b704ed95c380bcc088a73fd2dce5e7b
Abu <abubakarabukar@gmail.com>
2023-01-02 23:40:30 -0500

    Update About section readability

--- src/Tabs/About/About.js
+++ src/Tabs/About/About.js
@@ -15,38 +15,43 @@ const About = () => {
             Trades are one of the most interesting parts about the NBA and can
             have a domino effect for future transactions to come. This
             application allows users to explore the history of NBA player
-            transactions by visualizing them as a graph. To build the graph,
-            simply select a player and (optionally) another player who has been
-            involved in a trade with them. You can also specify the number of
-            additional exchanges to include in the graph, allowing you to
-            control its size and complexity.
+            transactions by visualizing them as a graph.
           </Text>
         </GridItem>
         <GridItem>
           <Text fontSize="xl">
-            Data source is from the amazing{" "}
+            To build the graph, simply select a player and (optionally) another
+            player who has been involved in a trade with them. You can also
+            specify the number of additional exchanges to include in the graph,
+            allowing you to control its size and complexity. Click the edges
+            between nodes for extra information!
+          </Text>
+        </GridItem>
+        <GridItem>
+          <Text fontSize="xl">
+            Data source is from the amazing{"  "}
             <Link href="https://prosportstransactions.com/" isExternal>
               prosportstransactions.com <ExternalLinkIcon mx="2px" />
-            </Link>
-            {" "}and is currently using data from 2021 as it is still in development.
+            </Link>{" "}
+            and is currently using data from 2021 as it is still in development.
           </Text>
         </GridItem>
         <GridItem>
           <Text fontSize="xl">
-            Inspired by this {" "}
+            Inspired by this{" "}
             <Link
               href="https://svitkin.shinyapps.io/bball-trade-network/"
               isExternal
             >
-              project{" "}
-              <ExternalLinkIcon mx="2px" />
+              project <ExternalLinkIcon mx="2px" />
             </Link>
           </Text>
         </GridItem>
         <GridItem>
           <Link href="https://github.com/aaabuk02/trade-net" isExternal>
             Source Code <ExternalLinkIcon mx="2px" />
-          </Link><br/>
+          </Link>
+          <br />
           <Link href="mailto:aaabuk02@gmail.com" isExternal>
             Contact Me <EmailIcon mx="2px" />
           </Link>
